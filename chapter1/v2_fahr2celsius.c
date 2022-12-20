c351bf965d62a8a95c7ce0af9e44f30b17492207
Alexander Cavalliotis <alexander.cavalliotis@gmail.com>
2022-12-20 22:01:07 +1100

    c-language learnings

--- chapter1/v2_fahr2celsius.c
+++ chapter1/v2_fahr2celsius.c
@@ -11,8 +11,9 @@ void main()
 	for (fahr = 0; fahr <= 300; fahr = fahr + 20) 
 		printf("%3d %6.1f\n", fahr, (5.0/9.0) * (fahr - 32.0));
 
+	printf("valentino-does'nt-exist\n\n");
 	// Ex 1-5, reverse order
-	for (fahr = 300; fahr > 0; fahr = fahr - 20) 
+	for (fahr = 300; fahr > -20; fahr = fahr - 20) 
 		printf("%3d %6.1f\n", fahr, (5.0/9.0) * (fahr - 32.0));
 	
 }
